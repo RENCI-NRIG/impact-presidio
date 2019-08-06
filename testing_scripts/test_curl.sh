7d70ee0f0b96229a53b404e55138d8b4f8e31188
Victor J. Orlikowski <vjo@duke.edu>
2019-08-06 10:24:06 -0400

    Update testing scripts to use GET rather than POST

--- testing_scripts/test_curl.sh
+++ testing_scripts/test_curl.sh
@@ -4,4 +4,4 @@ KEY=key.pem
 CERT=cert.pem
 CAFILE=ca-certs.pem
 
-curl -v -L -c cookie_file --data "ImPACT-JWT=${IMPACT_JWT}" --key "${KEY}" --cert "${CERT}" --cacert "${CAFILE}" "${TEST_URL}"
+curl -v -G -L -c cookie_file --data "ImPACT-JWT=${IMPACT_JWT}" --key "${KEY}" --cert "${CERT}" --cacert "${CAFILE}" "${TEST_URL}"
